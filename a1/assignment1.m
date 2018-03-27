b1cb15d59dd5d2fa5329776505cfa1b4f1210e8d
Wesley <wv.hoorn@student.ru.nl>
2018-03-27 13:44:51 +0200

    sbox

--- a1/assignment1.m
+++ a1/assignment1.m
@@ -2,11 +2,15 @@ load in.mat
 load traces.mat
 
 k = cast(0:15, "uint8")
+sbox = cast([12, 5, 6, 11, 9, 0, 10, 13, 3, 14, 15, 8, 4, 7, 1, 2], 'uint8')
 
-for i = 1:rows(k')
-  y(:,i) = sbox(bitxor(in,k(1,i))) 
+for i = 1:columns(k)
+  y(:,i) = bitxor(in,k(1,i)) 
 endfor
 
-function y = sbox(x)
-  
-endfunction
+for i = 1:rows(y)
+  for j = 1:columns(y)
+    y(i,j) = sbox(1, y(i,j)+1)
+  endfor
+endfor
+
