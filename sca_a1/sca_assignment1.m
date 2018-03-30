bf7cd7eddd433c5c800a23b7c1aeea9a7b0463ee
Wesley <wv.hoorn@student.ru.nl>
2018-03-30 21:06:48 +0200

    sorting works now

--- sca_a1/sca_assignment1.m
+++ sca_a1/sca_assignment1.m
@@ -20,8 +20,10 @@ pow_pred = reshape(sum(dec2bin(y)' == '1'), rows(y), columns(y));
 # step 5
 abs_cw_corr = abs(corr(traces, pow_pred));
 # step 6
-[max_corr key_idx] = max(max(abs_cw_corr));
-result = can_key(key_idx)
+can_key = cast(vertcat(0:15, max(abs_cw_corr))', "single");
+can_key = flipud(sortrows(can_key,2));
+printf "Key candidate with highest "
+correlation = result = can_key(1,1)
 
 # step 7
 figure;
@@ -31,7 +33,7 @@ title("Correlation between measured traces and predicted powerconsumption");
 
 hold on
 for idx = 1:columns(abs_cw_corr)
-  if(idx == key_idx)
+  if(idx == result+1)
     plot(abs_cw_corr(:,idx), 'Color',[1 0 0]);
   else
     plot(abs_cw_corr(:,idx), 'Color',[0 0 0]+0.025*idx);
@@ -41,18 +43,20 @@ hold off
 
 # step 8
 nr_of_traces = [ 500, 1000, 2000, 4000, 8000, 12000];
-labels = can_key;
 
 figure;
 for idx = 1:columns(nr_of_traces)
   subplot(3, 2, idx);
   sub_max_abs_cw_corr = max(abs(corr(traces(1:nr_of_traces(idx),:), pow_pred(1:nr_of_traces(idx),:))));
+  sub_can_key = vertcat(0:15, sub_max_abs_cw_corr)';
+  sub_can_key = flipud(sortrows(sub_can_key,2));
   
-  bar(sub_max_abs_cw_corr, 'facecolor', [0 0 0]);
+  bar(sub_can_key(:,2), 'facecolor', [0 0 0]);
   
-  axis([0, 17, 0.02, 0.2]);
-  set(gca, 'XTick', 1:length(labels));
-  set(gca, 'XTickLabel', labels);
+  l = legend ("best candidate = 6");
+  legend(l, "location", "southoutside");
+  xt = get(gca, 'XTick');
+  set(gca, 'XTick', xt, 'XTickLabel', sub_can_key(:,1));
   xlabel("Key candidates");
   ylabel("Correlation");
   title([num2str(nr_of_traces(idx)) " traces"]);
