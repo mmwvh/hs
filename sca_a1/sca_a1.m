e6bdb42fe6415294b51f73411a8dbc1a2a9dd8f0
Wesley <wv.hoorn@student.ru.nl>
2018-04-02 13:13:53 +0200

    first assignment finished and handed in

--- sca_a1/sca_a1.m
+++ sca_a1/sca_a1.m
@@ -7,12 +7,12 @@ load in.mat
 load traces.mat
 
 # step 2
-can_key = cast(0:15, "uint8");
+key_can = cast(0:15, "uint8");
 
 sbox = [12, 5, 6, 11, 9, 0, 10, 13, 3, 14, 15, 8, 4, 7, 1, 2];
 
-for idx = 1:sizeof(can_key)
-  y(:,idx) = sbox(bitxor(in,can_key(idx))+1); 
+for idx = 1:sizeof(key_can)
+  y(:,idx) = sbox(bitxor(in,key_can(idx))+1); 
 endfor
 
 # step 3
@@ -20,10 +20,10 @@ pow_pred = reshape(sum(dec2bin(y)' == '1'), rows(y), columns(y));
 # step 5
 abs_cw_corr = abs(corr(traces, pow_pred));
 # step 6
-can_key = cast(vertcat(0:15, max(abs_cw_corr))', "single");
-can_key = flipud(sortrows(can_key,2));
+key_can = cast(vertcat(0:15, max(abs_cw_corr))', "single");
+key_can = flipud(sortrows(key_can,2));
 printf "Key candidate with highest "
-correlation = result = can_key(1,1)
+correlation = result = key_can(1,1)
 
 # step 7
 figure;
@@ -42,21 +42,32 @@ endfor
 hold off
 
 # step 8
+# We had trouble coming up with a solution to color only one bar in the bar graph
+# For instance the following should have colored the third bar red:
+# 
+# b = bar(sub_key_can(:,2), 'facecolor', [0 0 0]);
+# set(b(3), 'facecolor', [1 0 0]); 
+#
+# but it did not because b would end up as a single double value instead of figure object.
+# So we added a legend that would show the best candidate.
+# This is not the most elegant way to do this, 
+# therefore we would like to know what the best way was to represent this.
+
 nr_of_traces = [ 500, 1000, 2000, 4000, 8000, 12000];
 
 figure;
 for idx = 1:columns(nr_of_traces)
   subplot(3, 2, idx);
   sub_max_abs_cw_corr = max(abs(corr(traces(1:nr_of_traces(idx),:), pow_pred(1:nr_of_traces(idx),:))));
-  sub_can_key = vertcat(0:15, sub_max_abs_cw_corr)';
-  sub_can_key = flipud(sortrows(sub_can_key,2));
+  sub_key_can = vertcat(0:15, sub_max_abs_cw_corr)';
+  sub_key_can = flipud(sortrows(sub_key_can,2));
   
-  bar(sub_can_key(:,2), 'facecolor', [0 0 0]);
+  bar(sub_key_can(:,2), 'facecolor', [0 0 0]);
   
   l = legend ("best candidate = 6");
-  legend(l, "location", "southoutside");
+  legend(l, "location", "southeast");
   xt = get(gca, 'XTick');
-  set(gca, 'XTick', xt, 'XTickLabel', sub_can_key(:,1));
+  set(gca, 'XTick', xt, 'XTickLabel', sub_key_can(:,1));
   xlabel("Key candidates");
   ylabel("Correlation");
   title([num2str(nr_of_traces(idx)) " traces"]);
