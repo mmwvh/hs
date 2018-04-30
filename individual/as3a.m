ede07edf9a0bc7878fa292146a4dcd040baccb69
Aniek <aniek.denteuling@gmail.com>
2018-04-30 12:17:44 +0200

    3a and 3b1 so far

--- individual/as3a.m
+++ individual/as3a.m
@@ -7,6 +7,7 @@ load dataset.mat
 t0 = mean(train_set0); 
 t1 = mean(train_set1);
 
+# STEP 1
 # Reduced template matching
 # Test set 0
 score0t0 = zeros(size(test_set0));
@@ -50,9 +51,94 @@ for idx = 1:numel(result1)
 endfor
 
 # Number of mismatches
-mismatch_t0 = sum(result0(:) == 1)
-mismatch_t1 = sum(result1(:) == 0)
+mismatch_t0 = sum(result0(:) == 1);
+mismatch_t1 = sum(result1(:) == 0);
 
 # Misclassification rate
 mis_clas_rate_t0 = mismatch_t0 / numel(result0)
 mis_clas_rate_t1 = mismatch_t1 / numel(result1)
+
+# STEP 2
+# POI using difference of means
+# Already computed the means of O_0 and O_1 in the beginning, namely t0 and t1
+mean_0 = t0;
+mean_1 = t1;
+difference_means = abs(mean_0 - mean_1);
+highest_difference = max(difference_means);
+
+index_high_diff = find(difference_means == highest_difference);
+poi_0 = t0(index_high_diff);
+poi_1 = t1(index_high_diff);
+
+var_0 = var(train_set0);
+var_1 = var(train_set1);
+
+# Check whether poi's match O_0 or O_1
+# Probability poi_0 comes from O_0
+p_0_0 = normpdf(poi_0, mean_0, var_0);
+# Probability poi_0 comes from O_1
+p_0_1 = normpdf(poi_0, mean_1, var_1);
+
+lambda_0 = p_0_0 / p_0_1
+if lambda_0 > 1
+  disp("POI_0 matches H1");
+else 
+  disp("POI_0 matches H0");
+end
+  
+# Probability poi_1 comes from O_0
+p_1_0 = normpdf(poi_1, mean_0, var_0);
+# Probability poi_1 comes from O_1
+p_1_1 = normpdf(poi_1, mean_1, var_1);
+
+lambda_1 = p_1_0 / p_1_1
+if lambda_1 > 1
+  disp("POI_1 matches H1");
+else
+  disp("POI_1 matches H0");
+end
+
+# Misclassification rate
+# STEP 3
+# (a) Compute mean of operation 0 and 1
+mean_op = 0.5 * (mean_0 + mean_1);
+
+# (b) Compute 1301 x 1301 matrix
+# Transposed all individual means, otherwise would end up with 1x1 matrix (1x1301 * 1301x1 = 1x1)
+b = 0.5 * (((mean_0.' - mean_op.') * (mean_0.' - mean_op.').') + ((mean_1.' - mean_op.') * (mean_1.' - mean_op.').'));
+
+# (c) Compute singular value decomposition
+[u,s,v] = svd(b);
+
+# (d) Compute u reduced
+# Chosen dimension: 30
+m = 500;
+u_reduced = u(:,1:m);
+
+# (e) Project datasets
+proj_test_0  = test_set0  * u_reduced;
+proj_test_1  = test_set1  * u_reduced;
+proj_train_0 = train_set0 * u_reduced;
+proj_train_1 = train_set1 * u_reduced;
+
+# (f) Compute multivariate templates of projected training sets
+mean_proj_train_0 = mean(proj_train_0);
+cov_train_0   = cov(proj_train_0);
+
+mean_proj_train_1 = mean(proj_train_1);
+cov_train_1   = cov(proj_train_1);
+
+# (g) Compute misclassification rate
+# My code does not work, I followed slide 37 from the FastTemplateTutorial pdf
+# but get stuck at the computation of lambda and gamma. However, here's the code:
+#{
+lambda_proj_train_0 = 0.5 * (proj_test_0 - mean_proj_train_0).' * inv(cov_train_0) ...
+          * (proj_test_0 - cov_train_0) - 0.5 * (proj_test_0 - mean_proj_train_1).' ...
+          * inv(cov_train_1) * (proj_test_0 - cov_train_1);
+
+lambda_proj_train_1 = 0.5 * (proj_test_1 - mean_proj_train_0).' * inv(cov_train_0) ...
+          * (proj_test_1 - cov_train_0) - 0.5 * (proj_test_0 - mean_proj_train_1).' ...
+          * inv(cov_train_1) * (proj_test_1 - cov_train_1);       
+
+gamma = 0.5 * log(det(cov_train_1)) - 0.5 * log(det(cov_train_0));
+#}
--- individual/as3b1.m
+++ individual/as3b1.m
@@ -3,23 +3,44 @@
 load input.mat
 load leakage_y0_y1.mat
 
-# Pre-processing step ????
-pre_pro = zeros(2000,45);
+# Pre-processing step 
+
+# I use function nchoosek which gives all combinations of vector v, picking 2 values at a time.
+# The vectors are the rows of the leakage matrix.
+# Each nchoosek(v,2) gives a matrix of 45x2 combinations but these need to be multiplied
+# to find the result of all combinations, which is a 45x1 vector, which I transpose to get 1x45 vectors.
+# These 2000 vectors together form the 2000x45 matrix with all possible multiplications between the samples.
+
 for idx = 1:rows(L)
-  pre_pro(idx, :) = nchoosek(1:columns(L), 2);
+  v = L(idx,:);
+  comb_matrix = nchoosek(v,2);
+  column1 = comb_matrix(:,1);
+  column2 = comb_matrix(:,2);
+  combinations = (column1 .* column2).';
+  pre_pro(idx,:) = combinations;
 endfor
 
+
 # Value prediction matrix
+# Compute all combinations of keys plus input values
 key_can = cast(0:15, "uint8");
 
-for idx 1:sizeof(key_can)
-  val_pred(:, idx) = bitxor(pre_pro, key_can(idx))+1);
+for idx = 1:sizeof(key_can)
+  val_pred(:, idx) = bitxor(input, key_can(idx));
 endfor
 
 # Correlation power analysis
 abs_corr_val = abs(corr(pre_pro, val_pred));
-
+# Result key
 key_can = cast(vertcat(0:15, max(abs_corr_val))', "single");
 key_can = flipud(sortrows(key_can,2));
 printf "Key candidate with highest "
 correlation = result = key_can(1,1)
+
+# Plot figure 
+figure;
+xlabel("Traces");
+ylabel("Correlation");
+title("Correlation of preprocessed traces and keys");
+
+hold on
